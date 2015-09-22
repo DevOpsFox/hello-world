5be54fac21c4ddec5db6f779533ff246d4162441
Alice Mallory <DevOpsFox@users.noreply.github.com>
2015-09-22 11:10:43 -0400

    Create Saluton.java

---
+++ Saluton.java
@@ -0,0 +1,10 @@
+// This is a modification of the "Saulton mondo!" example from Java in 24 Hours.
+
+package com.2048bit;
+
+class Saluton (
+    public static void main (String[] arguments) {
+        String greeting = "Saluton mondo!";
+        System.out.println(greeting); 
+    }
+}
