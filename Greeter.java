7625046536a8deb6dc4344412cf9948c76e85448
Alice Mallory <DevOpsFox@users.noreply.github.com>
2015-09-22 11:14:17 -0400

    Create Greeter.java

---
+++ Greeter.java
@@ -0,0 +1,12 @@
+// The first complete class for CCPS 109.
+
+public class Greeter {
+
+    public void greet() {
+        System.out.println("Hello, world!");
+    }
+    
+    public void bye() {
+        System.out.println("See you later!");
+    }
+}
