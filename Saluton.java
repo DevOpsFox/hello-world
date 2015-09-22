98b441779ccb811536c719ea64832de42587710a
Alice Mallory <DevOpsFox@users.noreply.github.com>
2015-09-22 11:48:41 -0400

    Update Saluton.java

--- Saluton.java
+++ Saluton.java
@@ -1,6 +1,6 @@
-// This is a modification of the "Saulton mondo!" example from Java in 24 Hours.
-
-package com.2048bit;
+*/ This is a modification of the "Saulton mondo!" example from Java in 24 Hours.
+*/
+package com.2048bit; //Arbitrary name of a package
 
 class Saluton (
     public static void main (String[] arguments) {
